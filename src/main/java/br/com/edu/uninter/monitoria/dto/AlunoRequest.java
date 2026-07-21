f99fa6304f36b0443123164b441a68ecd83c43d5
Ryan Martins <ryanbarroso2017@gmail.com>
2026-07-21 16:49:36 -0300

    fix(dto): Materia is not required anymore

--- src/main/java/br/com/edu/uninter/monitoria/dto/AlunoRequest.java
+++ src/main/java/br/com/edu/uninter/monitoria/dto/AlunoRequest.java
@@ -2,7 +2,6 @@ package br.com.edu.uninter.monitoria.dto;
 
 import br.com.edu.uninter.monitoria.model.TipoAluno;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.Set;
 
@@ -15,7 +14,6 @@ public record AlunoRequest(
         @NotNull(message = "O tipo do aluno é obrigatório")
         TipoAluno tipo,
 
-        @NotEmpty(message = "Informe ao menos uma matéria")
         Set<Long> materiaIds,
 
         Set<String> disponibilidade
