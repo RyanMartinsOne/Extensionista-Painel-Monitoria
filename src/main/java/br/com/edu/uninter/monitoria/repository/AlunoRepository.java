20fddb734a2f973713847d38c01498c61bd707f1
Ryan Martins <ryanbarroso2017@gmail.com>
2026-07-22 09:29:23 -0300

    feat(materia): Creating a service for Materia and a new existsByMaterias_Id method to ensure it is not linked to any Aluno before deletion.

--- src/main/java/br/com/edu/uninter/monitoria/repository/AlunoRepository.java
+++ src/main/java/br/com/edu/uninter/monitoria/repository/AlunoRepository.java
@@ -12,10 +12,12 @@ public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
     List<Aluno> findByTipo (TipoAluno tipo);
 
-    // Distinc evita repetir aluno mesmo se ele tiver várias materias
+    // Distinct evita repetir aluno mesmo se ele tiver várias materias
     @Query("SELECT DISTINCT a FROM Aluno a LEFT JOIN FETCH a.materias WHERE a.tipo = :tipo")
     List<Aluno> findByTipoWithMaterias(@Param("tipo") TipoAluno tipo);
 
     @Query("SELECT DISTINCT a FROM Aluno a JOIN a.materias m WHERE m.id = :materiaId")
     List<Aluno> findByMateriaId(@Param("materiaId") Long materiaId);
+
+    boolean existsByMaterias_Id(Long materiaId);
 }
---
+++ src/main/java/br/com/edu/uninter/monitoria/service/MateriaService.java
@@ -0,0 +1,78 @@
+package br.com.edu.uninter.monitoria.service;
+
+import br.com.edu.uninter.monitoria.dto.MateriaDTO;
+import br.com.edu.uninter.monitoria.dto.MateriaResponse;
+import br.com.edu.uninter.monitoria.mapper.MateriaMapper;
+import br.com.edu.uninter.monitoria.model.Materia;
+import br.com.edu.uninter.monitoria.repository.AlunoRepository;
+import br.com.edu.uninter.monitoria.repository.EncontroRepository;
+import br.com.edu.uninter.monitoria.repository.MateriaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class MateriaService {
+
+    private final MateriaRepository materiaRepository;
+    private final EncontroRepository encontroRepository;
+    private final AlunoRepository alunoRepository;
+    private final MateriaMapper materiaMapper;
+
+    @Transactional(readOnly = true)
+    public MateriaResponse listarPorId(Long id) {
+        Materia materia = materiaRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Não existe materia com id: " + id));
+
+        return materiaMapper.toDto(materia);
+    }
+
+    @Transactional(readOnly = true)
+    public List<MateriaResponse> listarTodos() {
+        List<Materia> materias = materiaRepository.findAll();
+
+        return materias.stream()
+                .map(materiaMapper::toDto)
+                .toList();
+    }
+
+    @Transactional
+    public MateriaResponse criarMateria(MateriaDTO materiaDTO) {
+        Materia materia = materiaMapper.toEntity(materiaDTO);
+        materiaRepository.save(materia);
+        return materiaMapper.toDto(materia);
+    }
+
+    @Transactional
+    public MateriaResponse atualizarMateria(Long id, MateriaDTO materiaDTO) {
+        Materia materia = materiaRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Não existe matéria com id: " + id));
+
+        materia.setNome(materiaDTO.nome());
+        materiaRepository.save(materia);
+        return materiaMapper.toDto(materia);
+    }
+
+    @Transactional
+    public void removerMateria(Long id) {
+        Materia materia = materiaRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Não existe matéria com id: " + id));
+
+        if (encontroRepository.existsByMateriaId(id)) {
+            throw new IllegalArgumentException("Não é possível excluir, esta matéria está em um ou mais encontros registrados.");
+        }
+
+        if (alunoRepository.existsByMaterias_Id(id)) {
+            throw new IllegalStateException(
+                    "Não é possível excluir: existem alunos vinculados a esta matéria."
+            );
+        }
+
+        materiaRepository.delete(materia);
+    }
+}
