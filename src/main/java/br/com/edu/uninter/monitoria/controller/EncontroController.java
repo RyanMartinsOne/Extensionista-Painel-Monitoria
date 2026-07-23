95da2a6ef2662d391b89833be449dc977ce7998a
Ryan Martins <ryanbarroso2017@gmail.com>
2026-07-23 09:59:47 -0300

    feat(controller): Creating controllers for Aluno, Materia and Encontro

---
+++ src/main/java/br/com/edu/uninter/monitoria/controller/EncontroController.java
@@ -0,0 +1,76 @@
+package br.com.edu.uninter.monitoria.controller;
+
+import br.com.edu.uninter.monitoria.dto.EncontroRequest;
+import br.com.edu.uninter.monitoria.dto.EncontroResponse;
+import br.com.edu.uninter.monitoria.model.StatusEncontro;
+import br.com.edu.uninter.monitoria.service.EncontroService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/encontros")
+@RequiredArgsConstructor
+public class EncontroController {
+
+    private final EncontroService encontroService;
+
+    @GetMapping
+    public ResponseEntity<List<EncontroResponse>> listar(
+            @RequestParam(required = false) StatusEncontro status
+    ) {
+        if (status != null) {
+            List<EncontroResponse> response = encontroService.listarPorStatus(status);
+            return ResponseEntity.ok(response);
+        }
+        List<EncontroResponse> response = encontroService.listarTodos();
+
+       return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EncontroResponse> listarPorId(@PathVariable Long id){
+        EncontroResponse encontroResponse = encontroService.listarPorId(id);
+        return ResponseEntity.ok(encontroResponse);
+    }
+
+    @GetMapping("/materia/{id}")
+    public ResponseEntity<List<EncontroResponse>> listarPorMateria(@PathVariable Long id){
+        List<EncontroResponse> encontroResponse = encontroService.listarPorMateriaId(id);
+        return ResponseEntity.ok(encontroResponse);
+    }
+
+    @GetMapping("/aluno/{id}")
+    public ResponseEntity<List<EncontroResponse>> listarPorAluno(@PathVariable Long id){
+        List<EncontroResponse> encontroResponse = encontroService.listarPorAluno(id);
+        return ResponseEntity.ok(encontroResponse);
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<Integer> quantidadeStatus(@RequestParam StatusEncontro status){
+        int response = encontroService.quantidadePorStatus(status);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<EncontroResponse> criar(@Valid @RequestBody EncontroRequest encontroRequest){
+        EncontroResponse response = encontroService.criar(encontroRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EncontroResponse> atualizar(@PathVariable Long id, @Valid @RequestBody EncontroRequest encontroRequest){
+        EncontroResponse response = encontroService.atualizar(id, encontroRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id){
+        encontroService.remover(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/br/com/edu/uninter/monitoria/controller/MateriaController.java
@@ -0,0 +1,50 @@
+package br.com.edu.uninter.monitoria.controller;
+
+import br.com.edu.uninter.monitoria.dto.MateriaRequest;
+import br.com.edu.uninter.monitoria.dto.MateriaResponse;
+import br.com.edu.uninter.monitoria.service.MateriaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/materia")
+@RequiredArgsConstructor
+public class MateriaController {
+
+    private final MateriaService materiaService;
+
+    @GetMapping
+    public ResponseEntity<List<MateriaResponse>> listar() {
+        List<MateriaResponse> response = materiaService.listarTodos();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MateriaResponse> listarPorId(@PathVariable Long id) {
+        MateriaResponse response = materiaService.listarPorId(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<MateriaResponse> salvar(@Valid @RequestBody MateriaRequest materiaRequest) {
+        MateriaResponse response = materiaService.criar(materiaRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MateriaResponse> atualizar(@PathVariable Long id, @Valid @RequestBody MateriaRequest materiaRequest) {
+        MateriaResponse response = materiaService.atualizar(id, materiaRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MateriaResponse> remover(@PathVariable Long id) {
+        materiaService.remover(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
