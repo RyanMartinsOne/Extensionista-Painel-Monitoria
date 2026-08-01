7eff286068957749e4149922e352fec33d631bbe
Ryan Martins <ryanbarroso2017@gmail.com>
2026-08-01 13:58:18 -0300

    feat(migrations): initial migrations

--- src/main/resources/db/migration/V2__add-foreign-keys.sql
+++ src/main/resources/db/migration/V2__add-foreign-keys.sql
@@ -1,17 +1,17 @@
 ALTER TABLE encontro
-    ADD CONSTRAINT FK_ENCONTRO_ON_BENEFICIADO FOREIGN KEY (beneficiado_id) REFERENCES aluno (id);
+    ADD CONSTRAINT FK_ENCONTRO_ON_BENEFICIADO FOREIGN KEY (beneficiado_id) REFERENCES usuario (id);
 
 ALTER TABLE encontro
     ADD CONSTRAINT FK_ENCONTRO_ON_MATERIA FOREIGN KEY (materia_id) REFERENCES materia (id);
 
 ALTER TABLE encontro
-    ADD CONSTRAINT FK_ENCONTRO_ON_MONITOR FOREIGN KEY (monitor_id) REFERENCES aluno (id);
+    ADD CONSTRAINT FK_ENCONTRO_ON_MONITOR FOREIGN KEY (monitor_id) REFERENCES usuario (id);
 
 ALTER TABLE aluno_materia
-    ADD CONSTRAINT fk_alumat_on_aluno FOREIGN KEY (aluno_id) REFERENCES aluno (id);
+    ADD CONSTRAINT fk_alumat_on_aluno FOREIGN KEY (aluno_id) REFERENCES usuario (id);
 
 ALTER TABLE aluno_materia
     ADD CONSTRAINT fk_alumat_on_materia FOREIGN KEY (materia_id) REFERENCES materia (id);
 
 ALTER TABLE aluno_disponibilidade
-    ADD CONSTRAINT fk_aluno_disponibilidade_on_aluno FOREIGN KEY (aluno_id) REFERENCES aluno (id);
+    ADD CONSTRAINT fk_aluno_disponibilidade_on_aluno FOREIGN KEY (aluno_id) REFERENCES usuario (id);
